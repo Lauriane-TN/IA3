91ea2288e97d36b04119872553db07320825e8f1
lauriane nathou <laurianenathou@gmail.com>
2022-11-29 13:48:53 +0100

    Exercice de distributeur de canettes - OOP

---
+++ week_2/can_machine/interface.py
@@ -0,0 +1,11 @@
+#Demander à l'utilisateur s'il veut acheter une canette ?
+#Afficher le prix
+#Demander confirmation
+#Remercier pour l'achat
+
+#Demander au technicien de s'identifier
+#Demander au technicien son choix
+    #afficher l'état du stock
+    #afficher la trésorie
+    #ajouter des cannettes au stock
+    #vider la caisse
---
+++ week_2/can_machine/machine.py
@@ -0,0 +1,8 @@
+class Machine:
+    def __init__(self):
+        pass
+# https://ressources.unisciel.fr/algoprog/s13ssprg/emodules/ss11mexerc1/res/ss11exerc1-cresolu-py-xxx.pdf
+#Initialiser un model machine et ses caractéristiques
+    #stock 100 cans
+    #account
+    #unit_price
